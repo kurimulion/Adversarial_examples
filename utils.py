9d3300cb7a4f25af9364a12f795845feec1d4d59
kurimulion <kurimulion@gmail.com>
2019-09-13 20:46:45 +0800

    merge normalization

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -182,3 +183,36 @@ def generator_loss_BCE(logits_fake):
     probs_fake = torch.sigmoid(logits_fake)
     loss = F.binary_cross_entropy(probs_fake, torch.ones_like(probs_fake))
     return loss
+
+# the module is taken from https://github.com/pytorch/pytorch/issues/8985#issuecomment-405080775
+class ConditionalBatchNorm2d(nn.Module):
+    def __init__(self, num_features, num_classes):
+        super(ConditionalBatchNorm2d, self).__init__()
+        self.num_features = num_features
+        self.bn = nn.BatchNorm2d(num_features, affine=False)
+        self.embed = nn.Embedding(num_classes, num_features * 2)
+        self.embed.weight.data[:, :num_features].normal_(1, 0.02)  # Initialise scale at N(1, 0.02)
+        self.embed.weight.data[:, num_features:].zero_()  # Initialise bias at 0
+
+    def forward(self, x, y):
+        out = self.bn(x)
+        y_ = y.type(torch.cuda.LongTensor)
+        gamma, beta = self.embed(y_).chunk(2, 1)
+        out = gamma.view(-1, self.num_features, 1, 1) * out + beta.view(-1, self.num_features, 1, 1)
+        return out
+    
+class ConditionalInstanceNorm2d(nn.Module):
+    def __init__(self, num_features, num_classes):
+        super(ConditionalInstanceNorm2d, self).__init__()
+        self.num_features = num_features
+        self.bn = nn.InstanceNorm2d(num_features, affine=False, track_running_stats=True)
+        self.embed = nn.Embedding(num_classes, num_features * 2)
+        self.embed.weight.data[:, :num_features].normal_(1, 0.02)  # Initialise scale at N(1, 0.02)
+        self.embed.weight.data[:, num_features:].zero_()  # Initialise bias at 0
+
+    def forward(self, x, y):
+        out = self.bn(x)
+        y_ = y.type(torch.cuda.LongTensor)
+        gamma, beta = self.embed(y_).chunk(2, 1)
+        out = gamma.view(-1, self.num_features, 1, 1) * out + beta.view(-1, self.num_features, 1, 1)
+        return out
